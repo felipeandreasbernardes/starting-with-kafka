f7465add01c7cf710d3bb6d24ca916a986e61686
Felipe Bernardes <felipe.bernardes@tecfinance.com.br>
2021-04-25 13:30:11 -0300

    initial commit

---
+++ KafkaConsumer/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using Confluent.Kafka;
+
+namespace KafkaConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                consumer.Subscribe("quickstart-events");
+
+                CancellationTokenSource cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) =>
+                {
+                    e.Cancel = true;
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var cr = consumer.Consume(cts.Token);
+                            System.Console.WriteLine($"Consumed message: '{cr.Value}' at: '{cr.TopicPartitionOffset}.'");
+                        }
+                        catch (ConsumeException ex)
+                        {
+                            Console.WriteLine($"Error occured: {ex.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException ex)
+                {
+                    consumer.Close();
+                }
+            }
+        }
+    }
+}
---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+
+namespace KafkaProducer
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    while (true)
+                    {
+                        System.Console.WriteLine("Insira uma mensagem para ser publicada!");
+                        var message = Console.ReadLine();
+                        var dr = await producer.ProduceAsync("quickstart-events", new Message<Null, string> { Value = message });
+                        System.Console.WriteLine($"Delivered: '{dr.Value}' to '{dr.TopicPartitionOffset}'");
+                    }
+                }
+                catch (ProduceException<Null, string> ex)
+                {
+                    Console.WriteLine($"Delivery failed: {ex.Error.Reason}");
+                }
+            }
+        }
+    }
+}
